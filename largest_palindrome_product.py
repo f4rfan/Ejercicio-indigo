eedcd2e8d8c0757546201a92e01618a93f557250
farfan <sergio_farfan_mp@hotmail.com>
2017-11-26 11:41:29 -0600

    Correciones

--- largest_palindrome_product.py
+++ largest_palindrome_product.py
@@ -1,7 +1,8 @@
 def main():
-    rsp = ''
+    rsp = ""
     resp = []
-    while rsp != 'N' and rsp != 'n':
+    nn = ["N","n"]
+    while rsp not in nn :
         tests = int(input())
         var = True
         while var is True:
@@ -15,29 +16,30 @@ def main():
         for i in resp:
             print(i)
             
-        rsp = input("Desea volver a intentarlo? \nS/N: ")
+        rsp = input("Desea volver a intentarlo?\nS/N:")
 
 
 def func():
-    n_char = input()
-    n = int(n_char)
+    n1 = int(input())
     serie = []
     valores = []
 
     var = True
     while var is True:
-        if 101101 > n or n > 1000000:
-            n = int(input("N debe ser mayor que 101101 y menor que 1000000: "))
+        if 101101 > n1 or n1 > 1000000:
+            n1 = int(input("N debe ser mayor que 101101 y menor que 1000000: "))
         else:
             var = False
 
     for i in range(100,1000):
         for x in range(100,1000):
-            if (i*x) <= n and (i*x) >= 101101:
+            valores =[]
+            if (i*x) <= n1 and (i*x) >= 101101:
                 valor = str(i*x)
                 if valor[:3] == valor[-1:-4:-1]:
                     if (i*x) not in serie:
-                        serie.append(i*x)
+                        valores = [i*x,i,x]
+                        serie.append(valores)
 
     serie.sort()
     
